bb1f49cf7d5c8ce7077e6af14a30762f67599772
Leonstadt <alfredo.ordonez@tecnologicoliceocristiano.edu.ec>
2024-07-11 20:43:18 -0500

    participacion en clase

--- ObtenerPromedio.py
+++ ObtenerPromedio.py
@@ -1,7 +1,7 @@
 #Elaborar un algoritmo que solicite dos numeros y muestre el promedio de ambos
 
-N1=int(input("Ingrese el número 1: "))
-N2=int(input("Ingrese el número 2: "))
+N1=int(input("Ingrese el numero 1: "))
+N2=int(input("Ingrese el numero 2: "))
 S=N1+N2
 P=S/2
-print(S)
+print(P)
